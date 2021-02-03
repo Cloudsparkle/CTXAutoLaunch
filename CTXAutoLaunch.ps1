c7726f736b0d1fed53fc85daeac707844340debd
Bart Jacobs <60138978+Cloudsparkle@users.noreply.github.com>
2021-02-03 16:43:37 +0100

    Create CTXAutoLaunch.ps1

---
+++ CTXAutoLaunch.ps1
@@ -0,0 +1,20 @@
+﻿param ($PublishedApp)
+
+$keys = Get-ChildItem -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall|Get-ItemProperty
+
+foreach ($key in $keys)
+{    
+    if (($key.Displayname -eq $PublishedApp) -or ($key.CTX_Displayname -eq $PublishedApp))
+    {
+        start-process -filepath $key.shortcuttarget -argumentlist "-showAppPicker"
+        Sleep 15
+        Start-Process -FilePath $key.shortcuttarget -ArgumentList $key.launchstring
+    }
+     
+    if ($key.CTX_Displayname -eq $PublishedApp)
+    {
+        start-process -filepath $key.shortcuttarget -argumentlist "-showAppPicker"
+        Sleep 15
+        Start-Process -FilePath $key.shortcuttarget -ArgumentList $key.launchstring
+    }
+}
