fa746dc545deaa97191d12100d6f6795c150bda6
Bart Jacobs <60138978+Cloudsparkle@users.noreply.github.com>
2021-02-16 09:58:19 +0100

    Update CTXAutoLaunch.ps1

--- CTXAutoLaunch.ps1
+++ CTXAutoLaunch.ps1
@@ -1,39 +1,56 @@
 param ($PublishedApp)
-# Info to be added
+<#
+.SYNOPSIS
+  Automatically launch a Citrix published resource
+.DESCRIPTION
+  This script provides a GUI to quickly clean an existing AD group from disabled user accounts, optionally recursive
+.PARAMETER PublishedApp
+  Name of the Citrix published resource
+.INPUTS
+  Name of Citrix published resource
+.OUTPUTS
+  None
+.NOTES
+  Version:        1.0
+  Author:         Bart Jacobs - @Cloudsparkle
+  Creation Date:  16/02/2021
+  Purpose/Change: Automatically launch a Citrix published resource
+ .EXAMPLE
+  None
+#>
+
+# Check if launched with paramter. If not, exiting.
 if ($PublishedApp -eq $null)
 {
-    Write-Host "No Published Resource given as argument... Exiting"
-    exit 0
+  Write-Host "No Published Resource given as argument... Exiting"
+  exit 0
 }
-$StartApp = $false
 
-#########################################################################
-#                        Add shared_assemblies                          #
-#########################################################################
+# Initialize variables
+$StartApp = $false
 $currentDir = [System.AppDomain]::CurrentDomain.BaseDirectory.TrimEnd('\')
 if ($currentDir -eq $PSHOME.TrimEnd('\'))
-	{
-		$currentDir = $PSScriptRoot
-	}
+{
+  $currentDir = $PSScriptRoot
+}
+# Currentdir is the actual running directory
 
+# Load libraries
 [System.Reflection.Assembly]::LoadWithPartialName('presentationframework') | Out-Null
-# Mahapps Library
 [System.Reflection.Assembly]::LoadFrom($currentdir + "\assembly\MahApps.Metro.dll") | Out-Null
 [System.Reflection.Assembly]::LoadFrom($currentdir + '\assembly\System.Windows.Interactivity.dll') | Out-Null
 
-#########################################################################
-#                            Splash Screen                              #
-#########################################################################
-    $hash = [hashtable]::Synchronized(@{})
-    $runspace = [runspacefactory]::CreateRunspace()
-    $runspace.ApartmentState = "STA"
-    $Runspace.ThreadOptions = "ReuseThread"
-    $runspace.Open()
-    $runspace.SessionStateProxy.SetVariable("hash",$hash)
-    $Pwshell = [PowerShell]::Create()
+#The splash-screen
+$hash = [hashtable]::Synchronized(@{})
+$runspace = [runspacefactory]::CreateRunspace()
+$runspace.ApartmentState = "STA"
+$runspace.ThreadOptions = "ReuseThread"
+$runspace.Open()
+$runspace.SessionStateProxy.SetVariable("hash",$hash)
+$Pwshell = [PowerShell]::Create()
 
-    $Pwshell.AddScript({
-    $xml = [xml]@"
+$Pwshell.AddScript({
+$xml = [xml]@"
      <Window
 	xmlns:Controls="clr-namespace:MahApps.Metro.Controls;assembly=MahApps.Metro"
 	xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
@@ -64,51 +81,47 @@ if ($currentDir -eq $PSHOME.TrimEnd('\'))
 </Window>
 "@
 
-    $reader = New-Object System.Xml.XmlNodeReader $xml
-    $hash.window = [Windows.Markup.XamlReader]::Load($reader)
-    $hash.LoadingLabel = $hash.window.FindName("LoadingLabel")
-    $hash.LoadingLabel.Content= "Loading"
-    $hash.window.ShowDialog()
+$reader = New-Object System.Xml.XmlNodeReader $xml
+$hash.window = [Windows.Markup.XamlReader]::Load($reader)
+$hash.LoadingLabel = $hash.window.FindName("LoadingLabel")
+$hash.LoadingLabel.Content= "Loading"
+$hash.window.ShowDialog()
 
 }) | Out-Null
 
-#########################################################################
-#                       Main Script                                     #
-#########################################################################
-
-#Launching Splash-screen
+# Launching Splash-screen
 $Pwshell.Runspace = $runspace
 $script:handle = $Pwshell.BeginInvoke()
+#Without 5 seconds of sleep, errors are thrown at closure
 sleep 5
 
+# Read all uninstall keys in the registry
 $keys = Get-ChildItem -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall|Get-ItemProperty
 
 foreach ($key in $keys)
 {
-    #Write-Host "Looping..."
-    #$hash.window.CurrentDispatcher.Invoke("Normal",[action]{ $hash.LoadingLabel.Content= "Loading"   })
-    if (($key.Displayname -eq $PublishedApp) -or ($key.CTX_Displayname -eq $PublishedApp))
-    {
-        #For Receiver/Workspace to login
-        start-process -filepath $key.shortcuttarget -argumentlist "-showAppPicker"
-        Sleep 30
-        #When match found, exit loop
-        $StartApp = $true
-        break
-    }
-
+  # Receiver uses Displayname, Workspace App uses CTX_Displayname
+  if (($key.Displayname -eq $PublishedApp) -or ($key.CTX_Displayname -eq $PublishedApp))
+  {
+    # For Receiver/Workspace to login
+    start-process -filepath $key.shortcuttarget -argumentlist "-showAppPicker"
+    # Wait for login to finish
+    Sleep 30
+    # When match found with parameter, exit loop
+    $StartApp = $true
+    break
+  }
 }
-#Closing splash-screen
+# Closing splash-screen
 $hash.window.Dispatcher.Invoke("Normal",[action]{ $hash.window.close() })
 $Pwshell.EndInvoke($handle) | Out-Null
 $runspace.Close() | Out-Null
 if ($StartApp)
 {
-    start-Process -FilePath $key.shortcuttarget -ArgumentList $key.launchstring
+  start-Process -FilePath $key.shortcuttarget -ArgumentList $key.launchstring
 }
 Else
 {
-
-    write-host "Published Application not found."
+  write-host "Published Application not found."
 }
 exit 0
